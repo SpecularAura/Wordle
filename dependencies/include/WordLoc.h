560519ac5ca1284328d25ff809325996171d0b59
AumK <aumwinay@gmail.com>
2022-07-14 18:35:09 +0530

    Completed WordLoc Class

--- dependencies/include/WordLoc.h
+++ dependencies/include/WordLoc.h
@@ -1,16 +1,29 @@
 #ifndef WORDLOC_H
 #define WORDLOC_H
+#include<string>
+#include<array>
+#include<algorithm>
+
+namespace wordsize
+{
+    inline constexpr int wordsize{5};
+}
 
 class WordLoc
 {
 private:
-    /* data */
+    std::array<int, wordsize::wordsize> find;
+    int yellowcount;
+    char chpos;
 public:
-    WordLoc(/* args */);
+    WordLoc(int initval = -1);
+    void SetFindChar(char ch);
+    void LetterPos(const std::string& str, char ch);
+    void SetYellow(const WordLoc& wordloc);
+    bool ShowYellow(int j);
+    void UpdateYellow();
     ~WordLoc();
 };
-
-
 
 
 #endif
--- dependencies/src/WordLoc.cpp
+++ dependencies/src/WordLoc.cpp
@@ -1,9 +1,67 @@
 #include "WordLoc.h"
 
-WordLoc::WordLoc(/* args */)
+WordLoc::WordLoc(int initval) : yellowcount{0}, chpos{'\0'}
 {
+    for(auto it = find.begin(); it != find.end(); it++)
+    {
+        *it = initval;
+    }
 }
 
+void WordLoc::SetFindChar(char ch)
+{
+    chpos = ch;
+}
+
+void WordLoc::LetterPos(const std::string& str, char ch)
+{
+    SetFindChar(ch);
+    for(int i=0; i < str.size(); i++) 
+    {
+        if(str[i] == ch)
+        {
+            find[i] = i;
+        }
+    }
+}
+
+void WordLoc::SetYellow(const WordLoc& wordloc)
+{
+    int notfoundcount{ static_cast<int>(std::count(find.begin(), find.end(), -1))};
+    int lettercount{static_cast<int>(find.size()) - notfoundcount};
+    int correctpos{0};
+    for (int i = 0; i < find.size(); i++)
+    {
+        if (find[i] == wordloc.find[i] && find[i] != -1)
+        {
+            correctpos++;
+        }
+        
+    }
+    yellowcount = lettercount - correctpos;
+}
+
+bool WordLoc::ShowYellow(int j) 
+{
+    bool isyellow;
+    bool infind;
+    for(auto element : find)
+    {
+        if (element != -1)
+        {
+            infind = true;
+        }
+    }
+    isyellow = infind && static_cast<bool>(yellowcount);
+    UpdateYellow();
+    return isyellow;
+}
+
+void WordLoc::UpdateYellow()
+{
+    yellowcount = yellowcount - 1;
+}
 WordLoc::~WordLoc()
 {
+    
 }
